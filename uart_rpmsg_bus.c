4c3f0bcac6fcee753b9f7355cd4451e92eb318cb
Maxime MERE <maxime.mere@st.com>
2019-06-03 09:26:54 +0200

    Update: uart_rpmsg_bus.c Put library in alphabetical order

--- uart_rpmsg_bus.c
+++ uart_rpmsg_bus.c
@@ -10,24 +10,18 @@
 
 #define pr_fmt(fmt) "%s: " fmt, __func__
 
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/slab.h>
-#include <linux/idr.h>
-#include <linux/jiffies.h>
-#include <linux/sched.h>
-#include <linux/wait.h>
-#include <linux/rpmsg.h>
-#include <linux/mutex.h>
-#include <linux/of_device.h>
-
 #include <linux/device.h>
 #include <linux/errno.h>
+#include <linux/idr.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
-#include <linux/types.h>
-
+#include <linux/rpmsg.h>
 #include <linux/serdev.h>
+#include <linux/slab.h>
+#include <linux/types.h>
 
 #include "rpmsg_internal.h"
 
